28d37cc4065a08fd13e104edb8ce251b2d77a7cd
Enkay256 <105401319+Enkay256@users.noreply.github.com>
2022-05-25 23:49:00 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+
+str1 = input("string1:")
+str2 = input("string2:")
+sorted_str1 = sorted(str1)
+sorted_str2 = sorted(str2)
+
+if sorted_str1 == sorted_str2:
+    print("true")
+else:
+    print("false")
+
